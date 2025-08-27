f909ca8f755b5347656329d636002b1320923216
Om-anand-0 <om.d3v.21@gmail.com>
2025-08-27 15:43:23 +0530

    Auto push 2025-08-27 15:43:23.885383

--- Auto_push.py
+++ Auto_push.py
@@ -11,11 +11,11 @@ os.chdir(REPO_PATH)
 # Step 2: Push to GitHub
 os.system('git add .')
 os.system(f'git commit -m "Auto push {datetime.datetime.now()}"')
-os.system('git push origin main')
+os.system('git push origin master')
 
 # Step 3: Call Hackathon API
-#repo = "Om-anand-0/Test-Samadhan"  # 👈 your repo name
-repo = input("Enter your github repo link (Https)")
+repo = "Om-anand-0/Test-Samadhan"  # 👈 your repo name
+#repo = input("Enter your github repo link (Https)")
 api_url = f"https://api.calmchase.com/hackathon/lastpush?repo=https://github.com/{repo}.git"
 
 headers = {
