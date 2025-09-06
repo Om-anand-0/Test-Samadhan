98c1ded2166ce797829cc16a37980f310f3e566b
Om-anand-0 <om.d3v.21@gmail.com>
2025-09-06 22:17:20 +0530

    Auto push 2025-09-06 22:17:20.084790

--- Day 06/backend/main.js
+++ Day 06/backend/main.js
@@ -1,54 +1,22 @@
+
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL;
 
-mongoose.connect(MONGO_URL)
-  .then(() => console.log("✅ Connected to MongoDB Atlas"))
-  .catch(err => console.error("❌ MongoDB connection error:", err));
+let students = [
+  { id: 1, name: "Alice", age: 20, course: "CSE" },
+  { id: 2, name: "Bob", age: 21, course: "ECE" },
+  { id: 3, name: "Charlie", age: 22, course: "ME" }
+];
 
-
-const noteSchema = new mongoose.Schema({
-  title: String,
-  content: String
-});
-const Note = mongoose.model("Note", noteSchema);
-
-
-app.get("/api/notes", async (req, res) => {
-  const notes = await Note.find();
-  res.json(notes);
+app.get("/api/students", (req, res) => {
+  res.json(students);
 });
 
-
-app.post("/api/notes", async (req, res) => {
-  const { title, content } = req.body;
-  const newNote = new Note({ title, content });
-  await newNote.save();
-  res.json(newNote);
-});
-
-
-app.put("/api/notes/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, content } = req.body;
-  const updated = await Note.findByIdAndUpdate(id, { title, content }, { new: true });
-  res.json(updated);
-});
-
-
-app.delete("/api/notes/:id", async (req, res) => {
-  const { id } = req.params;
-  await Note.findByIdAndDelete(id);
-  res.json({ message: "Note deleted" });
-});
-
-app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- Day 06/src/main.tsx
+++ Day 06/src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Robot from './components/Robot.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <Robot/>
+    <App />
   </StrictMode>,
 )
